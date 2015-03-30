3175c5180933f0c7d048e5c402f04c06f43f7afa
Bruce Wang <number5@gmail.com>
2015-03-31 00:43:14 +1100

    customisations

---
+++ modules/overrides/init.zsh
@@ -0,0 +1,22 @@
+#!/usr/bin/env zsh
+
+setopt EXTENDED_GLOB NULL_GLOB
+
+sourcedir() {
+    if [ ! -d "$1" ]; then
+        echo "directory $1 not exists!"
+        return 1
+    fi
+    for f in "$1"/[0-9][0-9]-*; do
+        case $f in 
+            *~|*.bak|*.old|*.sw?|*.zwc|*.disabled) continue;;
+        esac
+        #echo "  sourcing file $f"
+        source "$f"
+    done
+}
+
+sourcedir ${0:a:h}/parts.d
+
+unsetopt NULL_GLOB
+# vim:ft=zsh
